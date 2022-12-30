427a5ce3f7dcc828f4f24df8d1c9f1b2715fd1fb
Leo <leo.a.alho@gmail.com>
2022-12-30 23:51:08 +0200

    Jälleen yksi yritys

--- health_check.sh
+++ health_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-RES=$(curl "https://misty-morning-3342.fly.dev/health")
-if   [ $RES=="ok" ]; then
+RES=$(curl -s https://misty-morning-3342.fly.dev/health)
+if   [ "$RES" == "ok" ]; then
     echo $RES
     exit 0
 else
